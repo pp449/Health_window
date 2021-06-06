e20ba65d007a03c9a8ba533f6aec7f0ad523e23b
pp449 <71641127+pp449@users.noreply.github.com>
2021-06-06 22:29:29 +0900

    made signup part

--- Frontend/src/views/login.js
+++ Frontend/src/views/login.js
@@ -1,26 +1,26 @@
 import React from "react";
 import { RiKakaoTalkFill } from "react-icons/ri";
 import { FaGithub } from "react-icons/fa";
-import "../scss/login.scss";
+import "../scss/sign.scss";
 
 function login() {
   return (
     <div className="wrapper">
-      <div className="login__text">로그인하기</div>
-      <div className="login">
-        <span className="login__ID">
+      <div className="sign__text">로그인하기</div>
+      <div className="sign">
+        <span className="sign__ID">
           아이디 <br />
           <form>
             <input type="text" />
           </form>
         </span>
-        <span className="login__PW">
+        <span className="sign__PW">
           패스워드 <br />
           <form>
             <input type="password" />
           </form>
         </span>
-        <button className="login__button">제출</button>
+        <button className="sign__button">제출</button>
       </div>
       <div className="social_login">
         <button className="kakao_login">
--- Frontend/src/views/signup.js
+++ Frontend/src/views/signup.js
@@ -1,26 +1,32 @@
 import React from "react";
 import { RiKakaoTalkFill } from "react-icons/ri";
 import { FaGithub } from "react-icons/fa";
-import "../scss/login.scss";
+import "../scss/sign.scss";
 
 function signup() {
   return (
     <div className="wrapper">
-      <div className="login__text">회원가입</div>
-      <div className="login">
-        <span className="login__ID">
+      <div className="sign__text">회원가입</div>
+      <div className="sign signup">
+        <span className="sign__ID">
           아이디 <br />
           <form>
-            <input></input>
+            <input type="text" />
           </form>
         </span>
-        <span className="login__PW">
+        <span className="sign__PW">
           패스워드 <br />
           <form>
-            <input></input>
+            <input type="password" />
           </form>
         </span>
-        <button className="login__button">제출</button>
+        <span className="sign__PW">
+          패스워드 확인 <br />
+          <form>
+            <input type="password" />
+          </form>
+        </span>
+        <button className="sign__button">제출</button>
       </div>
       <div className="social_login">
         <button className="kakao_login">
